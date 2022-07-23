1eec12ac835c815f8049bcd73a700dfb39caf574
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-23 02:04:33 -0300

    creating the list

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -38,4 +38,4 @@ class Form extends React.Component {
     }
 }
 
-export default Form
+export default Form;
---
+++ src/components/list/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function List() {
+    return (
+        <aside>
+            <h2>Studies of the day</h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                        02:30:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        Javascript
+                    </h3>
+                    <span>
+                        01:10:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
