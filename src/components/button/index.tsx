983b4eb031dc005e403727efb7f6f611d06741cc
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-23 01:20:28 -0300

    first component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class button extends React.Component {
+    render() {
+        return (
+            <button>
+                botao
+            </button>
+        )
+    }
+}
+
+export default button
