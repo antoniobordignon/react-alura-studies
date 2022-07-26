187e3c59ab0a3d7328c4a97d719d7a650541a5fa
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-26 18:05:19 -0300

    controlling the form

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,10 +3,20 @@ import Button from '../button';
 import style from './Form.module.scss';
 
 
-class Form extends React.Component { 
+class Form extends React.Component {
+    state = {
+        task: "", 
+        time: "00:00"
+    }
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log('state: ',this.state);
+    }
+
     render() {
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">
                         add a new study
@@ -15,6 +25,8 @@ class Form extends React.Component {
                         type="text"
                         name="task"
                         id="task"
+                        value={this.state.task}
+                        onChange={event => this.setState({ ...this.state, task: event.target.value})}
                         placeholder="what do you want to learn?"
                         required
                     />
@@ -27,6 +39,8 @@ class Form extends React.Component {
                         type="time"
                         step="1"
                         name="time"
+                        value={this.state.time}
+                        onChange={event => this.setState({ ...this.state, time: event.target.value})}
                         id="time"
                         min="00:00:00"
                         max="01:30:00"
