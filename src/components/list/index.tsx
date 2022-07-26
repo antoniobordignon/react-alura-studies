069c1bfaec97f9b0cb394955414fd39dcc8080b9
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-26 18:31:15 -0300

    added task

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,23 +1,12 @@
-import React, {useState}from 'react';
+import React from 'react';
+import { ITask } from '../../types/task';
 import Item from './item';
 import style from './List.module.scss';
 
-function List() {
-    const [tasks, setTasks] = useState([{
-        task: 'React', 
-        time: '02:00:00'
-    }, {
-        task: 'Javascript',
-        time: '01:10:00'
-    }, {
-        task: 'Typescript',
-        time: '03:00:00'
-    }]);
+function List({tasks}: {tasks: ITask[]}) {
     return (
         <aside className={style.tasksList}>
-            <h2 onClick={() => {
-                setTasks ([...tasks, {task: "study state", time: "05:00:00"}])
-            }}>Studies of the day</h2>
+            <h2>Studies of the day</h2>
             <ul>
                 {tasks.map((item, index) => (
                   <Item
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../components/form';
 import List from '../components/list';
 import style from './App.module.scss';
 import Timer from '../components/timer';
+import { ITask } from '../types/task';
+
+
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
   return (
     <div className={style.AppStyle}>
-      <Form />
-      <List />
+      <Form setTasks={setTasks}/>
+      <List tasks={tasks} />
       <Timer />
     </div>
   );
