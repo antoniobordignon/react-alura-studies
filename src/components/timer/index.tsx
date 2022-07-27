fb757f30ac080a67511340ee418303f289222c13
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-27 16:24:27 -0300

    watching changes

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -3,7 +3,7 @@ import Clock from './clock'
 import style from './Timer.module.scss'
 import { timeToSeconds } from '../../common/utils/time';
 import { ITask } from '../../types/task';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Props {
     selected: ITask | undefined
@@ -11,13 +11,17 @@ interface Props {
 
 export default function Timer({ selected}: Props) {
     const [time, setTime] = useState<number>();
-    if(selected?.time) {
-        setTime(timeToSeconds(selected.time));
-    }
+
+    useEffect(() => {
+        if(selected?.time) {
+            setTime(timeToSeconds(selected.time));
+        }
+    },[selected])
+
     return (
         <div className={style.timer}>
             <p className={style.title}>choose a card and start the timer</p>
-            Time: {time}
+            Time:{time}
             <div className={style.clockWrapper}>
                 <Clock/>
             </div>
