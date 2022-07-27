5d6a7a79a16b3a578bf1f3383c7ccc8a86e03229
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-26 22:01:09 -0300

    updating the task

--- src/components/list/item/index.tsx
+++ src/components/list/item/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ITask } from '../../../types/task';
 import style from '../List.module.scss';
 
-export default function Item ({task, time}: {task: string, time: string}) {
+export default function Item ({task, time, selected, completed, id }: ITask) {
+    console.log('item: ', {task, time, selected, completed, id });
     return (
         <li className={style.item}>
             <h3>{task}</h3>
