d1c30a2f04808a9143cdb360555fb4d39ff2e64d
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-27 15:41:47 -0300

    timetoseconds

--- src/components/list/item/index.tsx
+++ src/components/list/item/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ITask } from '../../../types/task';
-import style from '../List.module.scss';
+import style from './Item.module.scss';
 
 interface Props extends ITask {
     selectTask: (selectedTask: ITask) => void
@@ -8,7 +8,6 @@ interface Props extends ITask {
   
 
 export default function Item ({task, time, selected, completed, id, selectTask }: Props) {
-    console.log('item: ', {task, time, selected,  completed, id });
     return (
         <li className={`${style.item} ${selected ? style.selectedItem: ''}`} onClick={() => selectTask({
             task, time, selected, completed, id
--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,8 +1,10 @@
 import Button from '../button'
 import Clock from './clock'
 import style from './Timer.module.scss'
+import { timeToSeconds } from '../../common/utils/date';
 
 export default function Timer() {
+    console.log('conversion: ', timeToSeconds('01:01:01'));
     return (
         <div className={style.timer}>
             <p className={style.title}>choose a card and start the timer</p>
