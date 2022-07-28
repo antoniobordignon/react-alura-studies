ec9f7bdf1a761330f3c4ee4eb07c1a8c1f1ac489
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-28 14:49:40 -0300

    finishing the task

--- src/components/list/item/index.tsx
+++ src/components/list/item/index.tsx
@@ -9,11 +9,12 @@ interface Props extends ITask {
 
 export default function Item ({task, time, selected, completed, id, selectTask }: Props) {
     return (
-        <li className={`${style.item} ${selected ? style.selectedItem: ''}`} onClick={() => selectTask({
+        <li className={`${style.item} ${selected ? style.selectedItem: ''} ${completed ? style.completedItem : ''}`} onClick={() => !completed && selectTask({
             task, time, selected, completed, id
         })}>
             <h3>{task}</h3>
             <span>{time}</span>
+            {completed && <span className={style.concluded} aria-Label="task completed"></span>} 
         </li>
     )
 }
